95643676ecee66adcbbf4830f245e2d8b96ddaae
guille.pares <guillepm09@gmail.com>
2026-03-23 20:14:40 +0100

    Borrar contenido de manager.py

--- api/manager.py
+++ api/manager.py
@@ -7,33 +7,7 @@ class TaskManager:
         self.db = db
 
     def create_task(self, data):
-
-        # Crear nueva tarea
-        nueva_tarea = Task(
-            titulo = data.titulo,
-            contenido = data.contenido,
-            deadline = data.deadline,
-            completada = False,
-            fecha_creacion = datetime.now()
-        )
-
-        # Guardar tarea en BBDD
-        self.db.add(nueva_tarea)
-        self.db.commit()
-        self.db.refresh(nueva_tarea)
-        self.db.close()
-
-        return {
-            "msg": "¡Tarea creada con éxito!",
-            "tarea": {
-                "id": nueva_tarea.id,
-                "titulo": nueva_tarea.titulo,
-                "contenido": nueva_tarea.contenido,
-                "deadline": nueva_tarea.deadline,
-                "completada": nueva_tarea.completada,
-                "fecha_creacion": nueva_tarea.fecha_creacion
-            }
-        }
+        pass
 
     def get_task_by_id(self, task_id):
         pass
