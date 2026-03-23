a1f19e88415dc309e935842828eb845c826eb93f
guille.pares <guillepm09@gmail.com>
2026-03-23 18:47:31 +0100

    Creada estructura de la clase TaskManager

--- api/manager.py
+++ api/manager.py
@@ -2,3 +2,15 @@ class TaskManager:
 
     def __init__(self, db):
         self.db = db
+
+    def create_task(self, data):
+        pass
+
+    def get_task_by_id(self, task_id):
+        pass
+    
+    def complete_task(self, task_id):
+        pass
+
+    def get_expired_tasks(self):
+        pass
