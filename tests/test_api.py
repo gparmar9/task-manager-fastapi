274b32741c66cffdc1b0cd1ab05a1e1d62db49fa
guille.pares <guillepm09@gmail.com>
2026-03-24 15:16:34 +0100

    Test crear_tarea compeltado

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,8 +5,20 @@ from datetime import datetime, date, timedelta
 BASE_URL = "http://localhost:8000"
 
 def test_crear_tarea():
-    """TODO: Implementar test para crear una tarea"""
-    pass
+    # Creamos el payload que manda la consulta como json
+    payload = {
+        "titulo": "Test crear tarea",
+        "contenido": "Probar el endpoint de crear tarea",
+        "deadline": "2026-03-26"
+    }
+
+    # Consulta post al endpoint correspondiente con el contenido json creado previamente
+    response = requests.post(f"{BASE_URL}/tasks/", json=payload)
+
+    # Comprobamos la respuesta de la consulta
+    assert response.status_code == 201, f"Esperado 201, obtenido {response.status_code}"
+    print("Test crear_tarea completado correctamente ✅")
+
 
 def test_obtener_tarea():
     """TODO: Implementar test para obtener una tarea por ID"""
@@ -26,5 +38,5 @@ def test_datos_incorrectos():
 
 if __name__ == "__main__":
     print("Ejecutando tests...")
-    # TODO: Llamar a las funciones de test y validar resultados
+    test_crear_tarea()
     print("Tests completados")
