a93b9ed1d748eb7ea914bf47d3649085d12d3aaf
guille.pares <guillepm09@gmail.com>
2026-03-24 12:43:00 +0100

    Logica de negocio movida de main.py a manager.py

--- api/main.py
+++ api/main.py
@@ -16,34 +16,13 @@ app = FastAPI(title="Gestión de tareas", version="1.0.0")
 # TODO: Implementar endpoints
 @app.post("/tasks/", response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
 def crear_tarea(task: TaskCreate):
-    
-    # Iniciar sesión de BBDD
+    # Abrimos sesión
     db = SessionLocal
 
-    # Crear tarea
-    # Crear nueva tarea
-    nueva_tarea = Task(
-        titulo = task.titulo,
-        contenido = task.contenido,
-        deadline = task.deadline,
-        completada = False,
-        fecha_creacion = datetime.now()
-    )
-
-    # Guardar tarea en BBDD
-    db.add(nueva_tarea)
-    db.commit()
-    db.refresh(nueva_tarea)
-    db.close()
-
-    return {
-        "id": nueva_tarea.id,
-        "titulo": nueva_tarea.titulo,
-        "contenido": nueva_tarea.contenido,
-        "deadline": nueva_tarea.deadline,
-        "completada": nueva_tarea.completada,
-        "fecha_creacion": nueva_tarea.fecha_creacion
-    }
+    # Instanciamos la clase TaskManager
+    manager = TaskManager(db)
+    nueva_tarea = manager.crear_tarea(task)
+    return nueva_tarea
 
 # @app.get("/tasks/{task_id}", response_model=TaskResponse)
 # def obtener_tarea(task_id: int):
--- api/manager.py
+++ api/manager.py
@@ -1,13 +1,38 @@
 from datetime import datetime
 from .models import Task
+from .database import engine, Base, SessionLocal
+from .schemas import TaskCreate, TaskUpdate, TaskResponse
 
 class TaskManager:
 
     def __init__(self, db):
         self.db = db
 
-    def create_task(self, data):
-        pass
+    def crear_tarea(self, task: TaskCreate):
+
+        # Crear nueva tarea
+        nueva_tarea = Task(
+            titulo = task.titulo,
+            contenido = task.contenido,
+            deadline = task.deadline,
+            completada = False,
+            fecha_creacion = datetime.now()
+        )
+
+        # Guardar tarea en BBDD
+        self.db.add(nueva_tarea)
+        self.db.commit()
+        self.db.refresh(nueva_tarea)
+        self.db.close()
+
+        return {
+            "id": nueva_tarea.id,
+            "titulo": nueva_tarea.titulo,
+            "contenido": nueva_tarea.contenido,
+            "deadline": nueva_tarea.deadline,
+            "completada": nueva_tarea.completada,
+            "fecha_creacion": nueva_tarea.fecha_creacion
+        }
 
     def get_task_by_id(self, task_id):
         pass
